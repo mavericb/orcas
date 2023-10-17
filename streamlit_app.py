88f37ab9898ac4642799006d6027c4a188384bfa
mavericb <127676723+mavericb@users.noreply.github.com>
2023-10-16 17:23:45 -0700

    first version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,53 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-import librosa
 import librosa.display
 import pandas as pd
-import numpy as np
+import datetime
+
+# Initialize session state
+if 'file' not in st.session_state:
+    st.session_state['file'] = None
+if 'annotations' not in st.session_state:
+    st.session_state['annotations'] = pd.DataFrame(columns=['Start', 'End', 'Annotation'])
+if 'annotation_list' not in st.session_state:
+    st.session_state['annotation_list'] = []
 
 # Load audio file
 file = st.file_uploader("Upload an audio file", type=["mp3"])
 if file is not None:
-    data, sr = librosa.load(file)
+    st.session_state['file'] = file
 
-    # Display waveform
-    fig, ax = plt.subplots(figsize=(14, 5))
-    librosa.display.waveshow(data, sr=sr, ax=ax)
-    st.pyplot(fig)
+if st.session_state['file'] is not None:
+    data, sr = librosa.load(st.session_state['file'])
+
+    # Play audio
+    st.audio(st.session_state['file'])
 
     # Annotate audio
-    start = st.number_input("Start time")
-    end = st.number_input("End time")
+    start = st.slider("Start time", min_value=0, max_value=int(len(data)/sr))
+    end = st.slider("End time", min_value=start, max_value=int(len(data)/sr))
     annotation = st.text_input("Annotation")
     if st.button("Submit"):
         new_annotation = pd.DataFrame({'Start': [start], 'End': [end], 'Annotation': [annotation]})
-        annotations = pd.concat([annotations, new_annotation], ignore_index=True)
+        st.session_state['annotations'] = pd.concat([st.session_state['annotations'], new_annotation], ignore_index=True)
+        st.session_state['annotation_list'].append((start, end, annotation))  # Add new annotation to the list
 
-        # Display annotations
-        st.write(annotations)
+    # Display waveform and add all annotations to the plot
+    fig, ax = plt.subplots(figsize=(14, 5))
+    librosa.display.waveshow(data, sr=sr, ax=ax)
+    for ann in st.session_state['annotation_list']:
+        ax.annotate(ann[2], ((ann[0] + ann[1]) / 2, 0), bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5))  # Adjust the position as needed
+        ax.fill_betweenx([-1, 1], ann[0], ann[1], color='red', alpha=0.3)  # Color the annotated part
+
+    # Redraw the plot to keep the old annotations
+    st.pyplot(fig)
+
+    # Display annotations
+    st.write(st.session_state['annotations'])
 
     # Export annotations
-    if st.button("Export annotations"):
-        annotations.to_csv('annotations.csv', index=False)
-        st.success("Annotations exported successfully!")
+    if st.button("Upload annotations"):
+        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        filename = f"annotations_{st.session_state['file'].name}_{timestamp}.csv"
+        st.session_state['annotations'].to_csv(filename, index=False)
+        st.success(f"Annotations uploaded successfully! The file is saved as {filename}")
